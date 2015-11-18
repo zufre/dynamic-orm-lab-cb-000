ed6b6680c853128b1b3ee6a390d4c5e184f5f700
joshuabamboo <joshuaowens011@gmail.com>
2015-11-18 14:05:14 -0500

    refactor #find_by

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,8 +53,9 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute_hash)
-    value = attribute_hash.values.first.class == Fixnum || Float ? attribute_hash.values.first : "'#{attribute_hash.values.first}'"
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{value}"
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum || Float ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
     DB[:conn].execute(sql)
   end
 end
