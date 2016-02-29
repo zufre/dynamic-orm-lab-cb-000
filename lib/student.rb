b6b4ba3c0835d8f78d5dd68edaebe346e4164cbd
joshuabamboo <joshuaowens011@gmail.com>
2016-02-29 18:03:20 -0500

    add solution to solution branch

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 end
