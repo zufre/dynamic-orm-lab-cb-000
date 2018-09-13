05378984865dbbd6e5e7866fea39fc4da7a6de0c
Cedric David <cedric.david@btopenworld.com>
2018-09-13 01:43:39 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -29,6 +29,7 @@ class InteractiveRecord
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
   def values_for_insert
+    values = []
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end  
