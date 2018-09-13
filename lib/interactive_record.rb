68d16cdad2ee2bd0264f2cf6f7be0cbeb22baf91
Cedric David <cedric.david@btopenworld.com>
2018-09-13 01:30:40 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -16,9 +16,7 @@ class InteractiveRecord
     end
     column_names.compact
   end
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
+  
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
